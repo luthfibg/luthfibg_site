3b0aede0368ed3054769bd1880565c58f0f1661b
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-28 11:24:47 +0700

    [#017] complete parallax section

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -1,8 +1,9 @@
 let guys = document.getElementById("sayhiguys");
-let mobile = document.getElementById("mobile");
+let text = document.getElementById("text");
 let computer = document.getElementById("computer");
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
-    guys.style.left = value * 2 + "px";
+    guys.style.left = value * 3 + "px";
+    text.style.top = value * 2 + "px";
 });
--- resources/views/components/headline.blade.php
+++ resources/views/components/headline.blade.php
@@ -1,9 +1,9 @@
 <div class="section headline-section row m-0 p-0">
-    <section class="parallax d-flex justify-content-center ">
-        <img src="assets/images/3d/astrobiology.png" alt="asset4" id="building" class="backward">
-        <img src="assets/images/3d/computer_3d.png" alt="asset1" id="computer">
-        <img src="assets/images/3d/mobile_3d.png" alt="asset2" id="mobile">
-        <img src="assets/images/3d/sayhiguys_3d.png" alt="asset3" id="sayhiguys">
+    <section class="parallax d-flex">
+        <img src="assets/images/3d/astrobiology.png" alt="asset4" id="lansdscape" class="backward">
+        {{-- <img src="assets/images/3d/computer_3d.png" alt="asset1" id="computer" class="comp"> --}}
+        <img src="assets/images/3d/welcome_text.png" alt="asset2" id="text" class="text">
+        <img src="assets/images/3d/sayhiguys_3d.png" alt="asset3" id="sayhiguys" class="guys">
         {{-- <img src="assets/images/3d/computer_3d.png" alt="" id="?"> --}}
     </section>
     {{-- <section class="sec">Lufhfibg</section> --}}

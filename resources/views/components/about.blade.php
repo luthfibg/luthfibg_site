066ef2b0c63ae0cd99578f8b6d63b4dbc8d6f64f
luthfibg <luthfinewzoneonline@gmail.com>
2023-03-01 15:01:06 +0700

    [#018] init about section, navbar bug

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,5 +1,29 @@
-<div class="section about-section row">
-    <div class="col-12">
-        <h1>This is About Part</h1>
+<div class="section about-section d-flex flex-row" id="about-section">
+    <div class="about-content">
+        <h3>Hello, It's Me</h3>
+        <h1>Muhamad Luthfi</h1>
+        <h3>And I'm a Web and Mobile Developer</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor delectus qui minima eos a nulla, sit doloremque magni numquam, sequi iste, esse ratione!</p>
+        <div class="social-medias">
+            <a href="">
+                <i class="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="">
+                <i class="fa-brands fa-youtube"></i>
+            </a>
+            <a href="">
+                <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="">
+                <i class="fa-brands fa-linkedin-in"></i>
+            </a>
+        </div>
+        <a href="http://">Download CV</a>
+    </div>
+    <div class="p-picture">
+        <img src="{{ asset('assets/photos/profile_blue.png') }}" alt="picture">
     </div>
 </div>
--- resources/views/components/headline.blade.php
+++ resources/views/components/headline.blade.php
@@ -1,4 +1,4 @@
-<div class="section headline-section row m-0 p-0">
+<div class="section headline-section m-0 p-0">
     <section class="parallax d-flex">
         <img src="assets/images/3d/astrobiology.png" alt="asset4" id="lansdscape" class="backward">
         {{-- <img src="assets/images/3d/computer_3d.png" alt="asset1" id="computer" class="comp"> --}}

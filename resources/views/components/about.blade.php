deb071bd04cbc7ed5cbe6e77c59ac4b56837d184
luthfibg <luthfinewzoneonline@gmail.com>
2023-03-01 17:01:26 +0700

    [#021] fit design to mobile first approach

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,6 +1,9 @@
-<div class="section about-section d-flex justify-content-center align-items-center" id="about-section">
-    <div class="about-content me-3 me-md-5">
-        <h3>Hello, Ini Saya</h3>
+<div class="section about-section d-flex justify-content-center align-items-center flex-column flex-md-row" id="about-section">
+    <div class="p-picture mt-1 mt-md-0 mb-5 mb-md-0 me-md-5">
+        <img src="{{ asset('assets/photos/profile_blue.png') }}" alt="picture">
+    </div>
+    <div class="about-content">
+        <h3>Halo, Ini Saya</h3>
         <h1>Muhamad Luthfi</h1>
         <h3>Saya Seorang <span>Web and Mobile Developer</span></h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor delectus qui minima eos a nulla, sit doloremque magni numquam, sequi iste, esse ratione!</p>
@@ -21,9 +24,6 @@
                 <i class="fa-brands fa-linkedin-in"></i>
             </a>
         </div>
-        <a href="http://" class="btn btn-cta mt-3 mt-md-5">Unduh CV</a>
-    </div>
-    <div class="p-picture">
-        <img src="{{ asset('assets/photos/profile_blue.png') }}" alt="picture">
+        <a href="http://" class="btn btn-cta mt-3 mt-lg-5">Unduh CV</a>
     </div>
 </div>

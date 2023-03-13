1b1902f063ea9b179eb40ff78290672bf6fe9c6c
luthfibg <luthfinewzoneonline@gmail.com>
2023-03-13 14:46:47 +0700

    [#27] home layout and btn adjust to mobile

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,9 @@
 <section class="home d-flex align-items-center" id="home">
-    <div class="home-content">
-        <h3>Halo, Saya</h3>
-        <h1>Muhamad Luthfi</h1>
+    <div class="home-content d-flex align-items-center justify-content-center flex-column">
+        <div class="title-subtitle d-flex flex-column justify-content-center align-items-start me-auto">
+            <h3>Halo, Saya</h3>
+            <h1>Muhamad Luthfi</h1>
+        </div>
         <p class="short-desc">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, neque. Praesentium possimus, sit ea nulla velit laborum molestiae quod, alias, sapiente quasi omnis aliquam magni? Suscipit tenetur aliquid voluptates laudantium cumque tempora repellat deleniti nemo labore!
         </p>
@@ -22,6 +24,6 @@
                 <i class="fa-brands fa-linkedin-in"></i>
             </a>
         </div>
-        <a href="http://">Download CV</a>
+        <a href="http://" class="btn mt-3">Download CV</a>
     </div>
 </section>

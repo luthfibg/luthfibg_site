e276ee5a8638af303a7c9c488c45d9a440090c28
luthfibg <luthfinewzoneonline@gmail.com>
2023-04-14 22:53:35 +0700

    [#63] fix headline content at the small screen

--- resources/views/components/headline.blade.php
+++ resources/views/components/headline.blade.php
@@ -1,4 +1,4 @@
-<section class="home d-flex align-items-center justify-content-between" id="main">
+<section class="home d-flex align-items-start justify-content-between" id="main">
     <div class="home-content d-flex justify-content-center flex-column">
         <div class="title-subtitle d-flex flex-column justify-content-center align-items-start">
             <h3>Halo, Saya</h3>
@@ -8,25 +8,50 @@
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, neque.
         </p>
         <div class="social-medias">
-            <a href="">
+            <a href="" data-tooltip-target="tooltip-f">
                 <i class="fa-brands fa-facebook-f"></i>
             </a>
-            <a href="">
+            <a href="" data-tooltip-target="tooltip-ig">
                 <i class="fa-brands fa-instagram"></i>
             </a>
-            <a href="">
+            <a href="" data-tooltip-target="tooltip-yt">
                 <i class="fa-brands fa-youtube"></i>
             </a>
-            <a href="">
+            <a href="" data-tooltip-target="tooltip-git">
                 <i class="fa-brands fa-github"></i>
             </a>
-            <a href="">
+            <a href="" data-tooltip-target="tooltip-lin">
                 <i class="fa-brands fa-linkedin-in"></i>
             </a>
+            <div id="tooltip-f" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                Facebook
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
+            <div id="tooltip-ig" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                Instagram
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
+            <div id="tooltip-yt" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                YouTube
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
+            <div id="tooltip-git" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                Github
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
+            <div id="tooltip-lin" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                Linkedin
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
         </div>
-        <a href="http://" class="btn mt-3">Download CV</a>
+        <a href="http://" class="btn mt-3" data-tooltip-target="tooltip-dark">
+            <i class="fa-solid fa-file-arrow-down"></i> CV</a>
+        <div id="tooltip-dark" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+            Download CV
+            <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
     </div>
-    <div class="profession-container">
+    {{-- <div class="profession-container">
         <div class="profession-box d-flex justify-content-center align-items-center p-1">
             <div class="profession" style="--i:0;">
                 <i class="fa-solid fa-code"></i>
@@ -48,7 +73,7 @@
             <div class="circle"></div>
         </div>
         <div class="overlay"></div>
-    </div>
+    </div> --}}
     <div class="home-image">
         <img src="{{ asset('assets/photos/my_img_cr.png') }}" alt="my-img">
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,9 @@
     {{-- Bootstrap 5.3 CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
+    {{-- Flowbite 1.6.5 CSS --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+
     {{-- Custom CSS --}}
     <link rel="stylesheet" href="{{ asset('css/theme.css') }}">
     <link rel="stylesheet" href="{{ asset('css/breakpoints.css') }}">
@@ -57,6 +60,7 @@
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 
     {{-- Custom JS --}}
     <script src="{{ asset('js/ctrl.js') }}"></script>

dcfa2745ae4f3403328ab1a9b1248c5b46f0bb9c
luthfibg <luthfinewzoneonline@gmail.com>
2023-05-04 00:20:58 +0700

    [#80] card flip login trial

--- resources/views/components/auth_card.blade.php
+++ resources/views/components/auth_card.blade.php
@@ -1,14 +1,32 @@
-<div class="card">
-    <h2 class="auth-title my-3 text-center">Autentikasi</h2>
-    <form action="https://" method="POST">
-        <div class="form-floating mb-3" class="fs-float">
-            <input type="email" class="form-control" name="email" id="floatingEmail" placeholder="Email">
-            <label for="floatingEmail">Alamat Email</label>
+<div class="cwr">
+    <input type="checkbox">
+    <div class="card">
+        <div class="frontside">
+            <h2 class="auth-title my-3 text-center">Autentikasi</h2>
+            <form action="https://" method="POST">
+                <div class="form-floating mb-3" class="fs-float">
+                    <input type="email" class="form-control" name="email" id="floatingEmail" placeholder="Email">
+                    <label for="floatingEmail">Alamat Email</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <span>Saya <a href="#">lupa password</a></span>
+                </div>
+                <button type="submit" name="submit_log" class="btn submit-login">{{ __('Login with email') }}</button>    
+            </form>
         </div>
-        <div class="form-floating mb-3">
-            <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
+        <div class="backside">
+            <h2 class="auth-title my-3 text-center">Lupa Password</h2>
+            <form action="https://" method="POST">
+                <div class="form-floating mb-3" class="fs-float">
+                    <input type="email" class="form-control" name="email" id="floatingEmail" placeholder="Email">
+                    <label for="floatingEmail">Alamat Email</label>
+                </div>
+                <button type="submit" name="submit_log" class="btn submit-login">{{ __('Send password change request') }}</button>    
+            </form>
         </div>
-        <button type="submit" name="submit_log" class="btn submit-login">{{ __('Login with email') }}</button>    
-    </form>
+    </div>
 </div>

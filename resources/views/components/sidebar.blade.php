0b569ece227bbd5cb41d070d896d907a7adfc809
luthfibg <luthfinewzoneonline@gmail.com>
2023-05-23 15:55:35 +0700

    [#92] advancing 1 sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -6,65 +6,68 @@
     </div>
     <ul class="nav-list">
         <li>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input class="form-control" type="text" placeholder="Cari...">
+            <span class="tooltip">Search</span>
+        </li>
+        <li>
             <a href="">
                 <i class="fa-solid fa-rocket"></i>
                 <span class="link-name">Dashboard</span>
             </a>
-            {{-- <span class="tooltip">Dashboard</span> --}}
+            <span class="tooltip">Menu utama</span>
         </li>
         <li>
             <a href="">
                 <i class="fa-solid fa-bell"></i>
-                <span class="link-name">Messages</span>
+                <span class="link-name">Pesan</span>
             </a>
-            {{-- <span class="tooltip">Dashboard</span> --}}
+            <span class="tooltip">Perpesanan</span>
         </li>
         <li>
             <a href="">
                 <i class="fa-solid fa-chart-simple"></i>
-                <span class="link-name">Analytics</span>
+                <span class="link-name">Analitik</span>
             </a>
-            {{-- <span class="tooltip">Dashboard</span> --}}
+            <span class="tooltip">Monitor data</span>
         </li>
         <li>
             <a href="">
                 <i class="fa-solid fa-folder"></i>
-                <span class="link-name">File Manager</span>
+                <span class="link-name">Kelola File</span>
             </a>
-            {{-- <span class="tooltip">Dashboard</span> --}}
+            <span class="tooltip">Pengelola berkas</span>
         </li>
         <li>
             <a href="">
                 <i class="fa-solid fa-cart-shopping"></i>
-                <span class="link-name">Order</span>
+                <span class="link-name">Pesanan</span>
             </a>
-            {{-- <span class="tooltip">Dashboard</span> --}}
+            <span class="tooltip">Pesanan</span>
         </li>
         <li>
             <a href="">
                 <i class="fa-solid fa-gear"></i>
-                <span class="link-name">Setting</span>
+                <span class="link-name">Pengaturan</span>
             </a>
-            {{-- <span class="tooltip">Dashboard</span> --}}
+            <span class="tooltip">Sesuaikan</span>
         </li>
         <li>
             <a href="">
                 <i class="fa-solid fa-user"></i>
-                <span class="link-name">User</span>
+                <span class="link-name">Pengguna</span>
             </a>
-            {{-- <span class="tooltip">Dashboard</span> --}}
+            <span class="tooltip">Detail pengguna</span>
         </li>
         <li class="profile">
             <div class="profile-details">
-                <img src="#" alt="profile image">
+                <img src="{{ asset('assets/photos/man1.jpg') }}" alt="profile image">
                 <div class="profile-content">
-                    <div class="name">Sekita Zu</div>
+                    <div class="name">Verda Salman</div>
                     <div class="designation">Admin</div>
                 </div>
             </div>
-            <a href="">
-                <i class="fa-solid fa-arrow-up-left-from-circle" id="logout"></i>
-            </a>
+            <i class="fa-solid fa-right-from-bracket" id="logout"></i>
         </li>
     </ul>
 </div>

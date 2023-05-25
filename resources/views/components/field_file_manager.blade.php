20c7c6221415ee9315ad04a45c01545ee4df4cc0
luthfibg <luthfinewzoneonline@gmail.com>
2023-05-26 04:56:56 +0700

    [#99] Setup user multirole

--- resources/views/components/field_file_manager.blade.php
+++ resources/views/components/field_file_manager.blade.php
@@ -13,7 +13,7 @@
             <a href="{{ route('downloadcv') }}" class="btn-sm btn-primary">
                 <i class="fa-solid fa-file-arrow-down fa-sm"></i>
             </a>
-            <a href="" class="disabled">
+            <a href="" class="undisabled">
                 <i class="fa-solid fa-lock-open fa-sm"></i>
             </a>
           </div>
@@ -31,7 +31,7 @@
             <a href="#" class="btn-sm btn-primary">
                 <i class="fa-solid fa-file-arrow-down fa-sm"></i>
             </a>
-            <a href="" class="disabled">
+            <a href="" class="undisabled">
                 <i class="fa-solid fa-lock-open fa-sm"></i>
             </a>
           </div>
@@ -41,6 +41,23 @@
   </div>
   <div class="row">
     <div class="col-12 col-sm-6 col-md-4 col-lg-3 mt-2 py-3">
+      @if (Auth::user()->role == 1)
+      <div class="card">
+        <div class="card-header">
+          CRUD File
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">CRUD File</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <div class="btn-action-wrapper">
+            <a href="#" class="btn btn-dashboard btn-primary">Kelola</a>
+            <a href="" class="undisabled">
+              <i class="fa-solid fa-lock-open fa-sm"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      @else
       <div class="card disabled">
         <div class="card-header">
           CRUD File
@@ -56,8 +73,27 @@
           </div>
         </div>
       </div>
+      @endif
+      
     </div>
     <div class="col-12 col-sm-6 col-md-4 col-lg-3 mt-2 py-3">
+      @if (Auth::user()->role == 1)
+      <div class="card">
+        <div class="card-header">
+          VVIF File
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">VVIF Reference</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <div class="btn-action-wrapper">
+            <a href="#" class="btn btn-dashboard btn-primary">Masuk</a>
+            <a href="" class="undisabled">
+              <i class="fa-solid fa-lock-open fa-sm"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      @else
       <div class="card disabled">
         <div class="card-header">
           VVIF File
@@ -73,6 +109,7 @@
           </div>
         </div>
       </div>
+      @endif
     </div>
   </div>
   <div class="row">
@@ -86,7 +123,7 @@
           <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
           <div class="btn-action-wrapper">
             <a href="#" class="btn btn-dashboard btn-primary">Buka</a>
-            <a href="" class="disabled">
+            <a href="" class="undisabled">
               <i class="fa-solid fa-lock-open fa-sm"></i>
             </a>
           </div>
